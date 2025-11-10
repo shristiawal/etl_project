e7cdd98b88bc065a7e4a8509f5c9cdf47505b2dd
Shristi Awal <shristiawal120@gmail.com>
2025-11-10 08:13:40 +0545

    updated main file

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("hello world!")
-print("new feature added")
+import mariadb
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+user_name = os.getenv('DB_USER')
+password = os.getenv('DB_password')
+
+# 1. Database Connection Parameters
+db_config = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': user_name,
+    'password': password,
+    'database': 'sakila'
+}
+conn =mariadb.connect(**db_config)
+cur=conn.cursor()
+cur.execute("SELECT*FROM FILM")
+res= cur.fetchall()
+print(res)
